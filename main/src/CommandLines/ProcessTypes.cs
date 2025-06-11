c2642dcf291a11b9a523443d6d1db35e4cf128b4
Yor-Jihons <ryo.malon814@gmail.com>
2025-06-11 15:48:35 +0900

    Init.

---
+++ main/src/CommandLines/ProcessTypes.cs
@@ -0,0 +1,13 @@
+namespace FormaGen.CommandLines
+{
+    /// <summary>
+    /// The types which indicates this app should do.
+    /// </summary>
+    public enum ProcessTypes
+    {
+        DefGeneration = 0,
+        InfoShow,
+        ListSdksShow,
+        Replacement
+    }
+}
---
+++ main/src/main.cs
@@ -0,0 +1,16 @@
+using System;
+using FormaGen.CommandLines;
+
+namespace FormaGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmdline = CmdLine.Create( args );
+            if( cmdline == null ) return;
+
+            Console.WriteLine( cmdline.ProcessTypes.ToString() );
+        }
+    }
+}
